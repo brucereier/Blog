c468671db9ef1993437b4ae2f37325f964c36a27
brucereier <brucereier2@gmail.com>
2025-02-01 11:49:53 -0500

    debug2

--- word-counter-backend/server.js
+++ word-counter-backend/server.js
@@ -135,12 +135,6 @@ app.get('/published-articles', async (req, res) => {
     const { importantArticles, nonImportantArticles } = await listPublishedArticles();
     const allArticles = [...importantArticles, ...nonImportantArticles];
     const articleKeys = allArticles.map(article => article.key);
-    try {
-      const readCounts = await getReadCounts(articleKeys);
-    } catch (error) {
-      console.error('Error in getReadCounts:', JSON.stringify({ articleKeys, error }, null, 2));
-      throw error;
-    }
     const readCountMap = readCounts.reduce((map, item) => {
       map[item.articleKey] = item.readCount || 0;
       return map;
